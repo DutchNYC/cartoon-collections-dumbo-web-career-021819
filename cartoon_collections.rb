fea4acb5e0263808fd17a8c8999198aa168a1c99
Matthew Lee <yleematthew@gmail.com>
2019-02-11 23:43:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,6 +3,7 @@ def roll_call_dwarves(dwarfname)# code an argument here
   dwarfname.each_with_index do |name, index|
     puts "#{index+1} {name}"
 end
+end
 
 def summon_captain_planet# code an argument here
   # Your code here
