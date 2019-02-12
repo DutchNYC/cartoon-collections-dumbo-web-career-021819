f734e2fd51fdf7e573ad1be2d031dff64c70b549
Matthew Lee <yleematthew@gmail.com>
2019-02-12 00:02:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,13 @@ def long_planeteer_calls(words)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)#code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.each do |type|
+    if cheese_types.include? type
+      return type
+    end
+  end
+  nil
 end
